ddf3d986d17278b0038420b541fae63544a412c1
david <dmsim159@gmail.com>
2025-05-10 22:20:39 -0700

    fix import error

--- agent_zero/main.py
+++ agent_zero/main.py
@@ -1,4 +1,4 @@
-from langchain_core import HumanMessage
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent
